18311f4d89ed2c7010434f5c20bfe79851ab17ba
Clayton Condie <Clayton Condie>
2016-01-22 12:21:31 -0700

    Adds small changes in javadoc for VertexObject

--- Catan/src/model/map/VertexObject.java
+++ Catan/src/model/map/VertexObject.java
@@ -1,5 +1,6 @@
 package model.map;
 
+import shared.locations.VertexLocation;
 
 /**
  * The VertexObject class is used represent either a settlement or a city
@@ -8,5 +9,10 @@ package model.map;
  */
 public class VertexObject 
 {
+	/** Represents the location of this vertexObject, normalized
+	VertexLocation location;
+	
+	/** Indicates the player to which this object is assigned */
+	int player;
 
 }
