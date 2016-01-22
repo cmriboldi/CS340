e97d5d592ece7e335668ee862ebe9e34f048f843
Clayton Condie <Clayton Condie>
2016-01-22 12:02:56 -0700

    Adds small changes to javadoc for Map class

--- Catan/src/model/map/Hex.java
+++ Catan/src/model/map/Hex.java
@@ -1,5 +1,8 @@
 package model.map;
 
+//Project Imports
+import shared.locations.HexLocation;
+
 /**
  * The Hex class is used to represent each tile on the board
  * @author Clayton
@@ -8,7 +11,15 @@ package model.map;
 
 public class Hex 
 {
+	/** String, possible enumeration, of the resource this hex provides */
 	String resource;
+	
+	/** The representation of the number tile associated with this hex */
 	int number;
+	
+	/** The representation of this tile's x,y coordinates. Most cases hexes will be accessed from
+	 * outside the Hex class, so this is more for reference
+	 */
+	HexLocation location;
 
 }
