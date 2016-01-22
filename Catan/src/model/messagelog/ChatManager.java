bb5e3f9ded041d5d81b819e1f26fb298783c0ec3
Joshua Powers <joshuapowers@Joshuas-MBP.app.byu.edu>
2016-01-22 14:17:57 -0700

    message log

---
+++ Catan/src/model/messagelog/ChatManager.java
@@ -0,0 +1,33 @@
+package model.messagelog;
+
+import java.util.List;
+
+public class ChatManager
+{
+	public ChatManager(){}
+	
+	List<String> chatMessages;
+	List<String> gameHistory;
+	
+	/**
+	 * update the chatMessages list
+	 * @param player sending message
+	 * @param string containing message
+	 * @return
+	 */
+	public void SendMessage()
+	{
+		
+	}
+	
+	/**
+	 * update the chatMessages list
+	 * @param player taking action
+	 * @param string with action taken message
+	 * @return
+	 */
+	public void LogAction()
+	{
+		
+	}
+}
