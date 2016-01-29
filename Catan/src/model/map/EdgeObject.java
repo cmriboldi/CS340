7b09343abfb4934649f9a0775f04b614e3c1d5d4
Clayton Condie <clayton.condie@gmail.com>
2016-01-29 10:24:19 -0700

    Adds EdgeObject Constructors.

--- Catan/src/model/map/EdgeObject.java
+++ Catan/src/model/map/EdgeObject.java
@@ -2,7 +2,9 @@ package model.map;
 
 //Project Imports
 
+import shared.locations.EdgeDirection;
 import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
 
 /**
  * Used to represent road states.
@@ -25,4 +27,24 @@ public class EdgeObject {
      */
     EdgeLocation location;
 
+    /**
+     * Independent constructor. Used when EdgeObject is not being stored in a map.
+     * @param x_t
+     * @param y_t
+     * @param dir
+     * @param owner_t
+     */
+    public EdgeObject(int x_t, int y_t, EdgeDirection dir, int owner_t) {
+        location = new EdgeLocation(new HexLocation(x_t, y_t), dir);
+        owner = owner_t;
+    }
+
+    /**
+     * Dependant constructor, used when storing EdgeObjects in a map of EdgeLocations
+     * @param owner_t
+     */
+    public EdgeObject(int owner_t) {
+        location = null;
+        owner = owner_t;
+    }
 }
