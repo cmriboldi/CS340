5b64a07537e90b858bc755883411ca6bf3775516
jvansteeter <jvansteeter@gmail.com>
2016-01-29 17:51:09 -0700

    created TestJSON class for testing

--- Catan/src/test/server/ProxyTestMain.java
+++ Catan/src/test/server/ProxyTestMain.java
@@ -2,10 +2,13 @@ package test.server;
 
 import java.util.List;
 
+import model.CatanModel;
+import serverProxy.JSONDeserializer;
 import serverProxy.RealProxy;
 import serverProxy.ServerException;
 import shared.communication.*;
 import shared.definitions.CatanColor;
+import test.TestJSON;
 
 public class ProxyTestMain 
 {
@@ -16,7 +19,7 @@ public class ProxyTestMain
 		{
 			System.out.println("Register new user");
 //			server.userRegister("test", "test");
-			server.userLogin("test", "test");
+			server.userLogin("Pete", "pete");
 			
 			System.out.println("Create new game");
 			CommGame newGame = server.createGame(true, true, true, "Automated Test");
@@ -33,7 +36,7 @@ public class ProxyTestMain
 					System.out.println("\tEmpty Player spot");
 				}
 			}
-			server.joinGame(3, CatanColor.RED);
+			server.joinGame(0, CatanColor.ORANGE);
 			server.getGameModel(1);
 			List<CommGame> games = server.listGames();
 			
@@ -69,6 +72,11 @@ public class ProxyTestMain
 			System.out.println("Reset Game");
 			server.resetGame();
 			System.out.println("Reset Successful!");
+			
+			System.out.println("Test Deserializer");
+			System.out.println(TestJSON.get());
+			CatanModel model = JSONDeserializer.deserialize(TestJSON.get());
+			System.out.println("Well it didn't throw any exceptions");
 		} 
 		catch (ServerException e) 
 		{
