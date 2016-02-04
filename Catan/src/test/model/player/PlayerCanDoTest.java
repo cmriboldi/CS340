ce824e58aed1afacf4d43542b0b9c5eff6f807bc
Jacob Brewer <jakebrucebrewer@gmail.com>
2016-02-03 18:08:12 -0700

    finish all player canDo methods on testJson object

--- Catan/src/test/model/player/PlayerCanDoTest.java
+++ Catan/src/test/model/player/PlayerCanDoTest.java
@@ -54,6 +54,12 @@ public class PlayerCanDoTest
 	public void testTurnTrackerInitialization()
 	{
 		
+		//turnTracker": {
+        //"status": "Rolling",
+        //"currentTurn": 0,
+        //"longestRoad": -1,
+        //"largestArmy": -1
+		
 		assertNotEquals(playerManager,null);
 		assertNotEquals(turnTracker,null);
 		assertNotEquals(playerManager.getCatanPlayers() ,null);
@@ -63,47 +69,76 @@ public class PlayerCanDoTest
 		assertNotEquals(playerManager.getTurnTracker().getTurnIndex(), -1); 
 		assertTrue(playerManager.getTurnTracker().getStatus().equals("Rolling")); 
 		assertFalse(playerManager.getTurnTracker().getStatus().equals("rolling")); 
-		
-		//turnTracker": {
-        //"status": "Rolling",
-        //"currentTurn": 0,
-        //"longestRoad": -1,
-        //"largestArmy": -1
 
 	}
 	
 	@Test
-	public void canPlay()
+	public void testCanPlay()
 	{
 		boolean player0CanPlay = options.canPlay(playerManager.getCatanPlayers()[0].getPlayerIndex());
 		assertFalse(player0CanPlay); // can't play because status is currently rolling
 		boolean player1CanPlay = options.canPlay(playerManager.getCatanPlayers()[1].getPlayerIndex());
 		assertFalse(player1CanPlay); 
-		boolean player2CanPlay = options.canPlay(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		boolean player2CanPlay = options.canPlay(playerManager.getCatanPlayers()[2].getPlayerIndex());
 		assertFalse(player2CanPlay);
-		boolean player3CanPlay = options.canPlay(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		boolean player3CanPlay = options.canPlay(playerManager.getCatanPlayers()[3].getPlayerIndex());
 		assertFalse(player3CanPlay);
 	}
 	
 	@Test
-	public void canRoll()
+	public void testCanRoll()
 	{
 		boolean player0CanRoll = options.canRollNumber(playerManager.getCatanPlayers()[0].getPlayerIndex());
 		assertTrue(player0CanRoll);  
 		boolean player1CanRoll = options.canRollNumber(playerManager.getCatanPlayers()[1].getPlayerIndex());
 		assertFalse(player1CanRoll);  
-		boolean player2CanRoll = options.canRollNumber(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		boolean player2CanRoll = options.canRollNumber(playerManager.getCatanPlayers()[2].getPlayerIndex());
 		assertFalse(player2CanRoll); 
-		boolean player3CanRoll = options.canRollNumber(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		boolean player3CanRoll = options.canRollNumber(playerManager.getCatanPlayers()[3].getPlayerIndex());
 		assertFalse(player3CanRoll); 
 	}
 	
-
+	@Test
+	public void canDiscardCards()
+	{
+		boolean player0CanDiscard = options.canDiscardCards(playerManager.getCatanPlayers()[0].getPlayerIndex());
+		assertFalse(player0CanDiscard);  
+		boolean player1CanDiscard = options.canDiscardCards(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		assertFalse(player1CanDiscard);  
+		boolean player2CanDiscard = options.canDiscardCards(playerManager.getCatanPlayers()[2].getPlayerIndex());
+		assertFalse(player2CanDiscard); 
+		boolean player3CanDiscard = options.canDiscardCards(playerManager.getCatanPlayers()[3].getPlayerIndex());
+		assertFalse(player3CanDiscard); 
+	}
 	
 	@Test
-	public void testCanDoMethods()
+	public void testCanFinishTurn()
 	{
-		
+		boolean player0CanFinish = options.canFinishTurn(playerManager.getCatanPlayers()[0].getPlayerIndex());
+		assertFalse(player0CanFinish);  
+		boolean player1CanFinish = options.canFinishTurn(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		assertFalse(player1CanFinish);  
+		boolean player2CanFinish = options.canFinishTurn(playerManager.getCatanPlayers()[2].getPlayerIndex());
+		assertFalse(player2CanFinish); 
+		boolean player3CanFinish = options.canFinishTurn(playerManager.getCatanPlayers()[3].getPlayerIndex());
+		assertFalse(player3CanFinish); 
 	}
+	
+	@Test
+	public void testCanPlaceRobber()
+	{
+		boolean player0CanPlaceRobber = options.canPlaceRobber(playerManager.getCatanPlayers()[0].getPlayerIndex());
+		assertFalse(player0CanPlaceRobber);  
+		boolean player1CanPlaceRobber = options.canPlaceRobber(playerManager.getCatanPlayers()[1].getPlayerIndex());
+		assertFalse(player1CanPlaceRobber);  
+		boolean player2CanPlaceRobber = options.canPlaceRobber(playerManager.getCatanPlayers()[2].getPlayerIndex());
+		assertFalse(player2CanPlaceRobber); 
+		boolean player3CanPlaceRobber = options.canPlaceRobber(playerManager.getCatanPlayers()[3].getPlayerIndex());
+		assertFalse(player3CanPlaceRobber); 
+	}
+	
+	
+	
+
 
 }
