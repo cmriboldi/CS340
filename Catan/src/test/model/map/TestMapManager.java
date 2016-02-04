471248198151dc1b9e5c606ffd28b45604d17c79
Clayton Condie <clayton.condie@gmail.com>
2016-02-03 18:46:02 -0700

    Clears TestMapManager

--- Catan/src/test/model/map/TestMapManager.java
+++ Catan/src/test/model/map/TestMapManager.java
@@ -6,48 +6,7 @@ import shared.exceptions.map.IllegalPlacementException;
 
 public class TestMapManager 
 {
-	@Test
-	public void testConstruct()
-	{
-		
-	}
-	
-	@Test
-	public void testFindLargestRoadValid()
-	{
-		
-	}
-	
-	@Test
-	public void testFindLargestRoadInvalid()
-	{
-		
-	}
-	
-	@Test
-	public void testPlaceRoadValid()
-	{
-		
-	}
-	
-	@Test(expected = IllegalPlacementException.class) 
-	public void testPlaceRoadInvalidLocation()
-	{
-		
-	}
-	
-	@Test
-	public void placeSettlementValid()
-	{
-		
-	}
-	
-	@Test
-	public void placeSettlementInvaldLocation()
-	{
-		
-	}
-	
+
 	
 
 }
