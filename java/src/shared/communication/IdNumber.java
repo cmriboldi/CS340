27c7845cce35c011cad2e3f846acd7731c702d99
jvansteeter <jvansteeter@gmail.com>
2016-02-23 23:18:57 -0700

    Returned IdNumber

---
+++ java/src/shared/communication/IdNumber.java
@@ -0,0 +1,31 @@
+package shared.communication;
+
+/**
+ * The IdNumber class contains user Id numbers.
+ * 
+ * @author Christian Riboldi
+ * @author Clayton Condie
+ * @author Jacob Brewer
+ * @author Joshua Powers
+ * @author Joshua Van Steeter
+ * @version 1.0 Build Winter 2016.
+ */
+public class IdNumber
+{
+	private int id;
+
+	public IdNumber(int playerId) {
+		this.id = playerId;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+}
