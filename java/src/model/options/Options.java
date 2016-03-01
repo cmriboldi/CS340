0e0854f52b5e9bfa830cd195e96d2faeb981b393
Clayton Condie <clayton.condie@gmail.com>
2016-02-29 17:54:37 -0700

    Adds mapMan canPlaceRobber into the options class

--- java/src/model/options/Options.java
+++ java/src/model/options/Options.java
@@ -187,7 +187,7 @@ public class Options
 	public boolean canPlaceRobber(HexLocation hex)
 	{
 		PlayerTurnTracker turnTracker = catanModel.playerManager.getTurnTracker();
-		return(Facade.getLocalPlayerIndex() == turnTracker.getTurnIndex() && turnTracker.getStatus().equals("Robbing"));
+		return(Facade.getLocalPlayerIndex() == turnTracker.getTurnIndex() && turnTracker.getStatus().equals("Robbing") && catanModel.getMapManager().canPlaceRobber(hex));
 	}
 	
 	public boolean canDiscardCards(int playerIndex)
