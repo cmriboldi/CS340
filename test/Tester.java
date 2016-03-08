24856c18376f0e99053017cfd1d0e3ec9945ecf2
jvansteeter <jvansteeter@gmail.com>
2016-03-07 23:06:25 -0700

    Ready to submit

--- test/Tester.java
+++ test/Tester.java
@@ -11,7 +11,7 @@ public class Tester {
         //"test.model.map.TestMapManager",
 		String[] testClasses = new String[]
 		{
-			"test.server.JSONDeserializerTest","test.server.MockProxyTest","test.server.PollerTest","test.server.RealProxyTest","test.model.development.DevCardListTest","test.model.development.DevCardManagerDeserialTest","test.model.development.DevCardManagerTest","test.model.development.PlayerDevCardsTest","test.model.map.TestMap","test.model.options.OptionsTest","test.model.player.PlayerCanDoTest","test.model.player.PlayerManagerDeserializerTest","test.model.resources.ResourceListTest","test.model.resources.ResourceManagerDeserialTest"
+			"test.server.JSONDeserializerTest","test.server.MockProxyTest","test.server.RealProxyTest","test.model.development.DevCardListTest","test.model.development.DevCardManagerDeserialTest","test.model.development.DevCardManagerTest","test.model.development.PlayerDevCardsTest","test.model.map.TestMap","test.model.options.OptionsTest","test.model.player.PlayerCanDoTest","test.model.player.PlayerManagerDeserializerTest","test.model.resources.ResourceListTest","test.model.resources.ResourceManagerDeserialTest"
 		};
 		//There is an error with this method when run in eclipse but it's used to run all of our tests from ant.
 		org.junit.runner.JUnitCore.main(testClasses);
