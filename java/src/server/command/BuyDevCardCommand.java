50bc2e713995a5a94f301b7a8497135c78495d06
Jacob Brewer <jakebrucebrewer@gmail.com>
2016-03-15 08:27:22 -0600

    add join game model logic

--- java/src/server/command/BuyDevCardCommand.java
+++ java/src/server/command/BuyDevCardCommand.java
@@ -16,11 +16,11 @@ public class BuyDevCardCommand implements ICommand {
 	}
 
 	/**
-	 *
-	 * Updates the CatanModel to reflect a player buying a dev card
-	 *
-	 * @return CatanModel, return the updated Catan model
-	 */
+	 * 
+	 * Updates the CatanModel to reflect buying a dev card
+	 * 
+     * @return CatanModel, return the updated Catan model
+     */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
--- java/src/server/command/RoadBuildingCommand.java
+++ java/src/server/command/RoadBuildingCommand.java
@@ -19,6 +19,7 @@ public class RoadBuildingCommand implements ICommand
      *
      * Updates the CatanModel to reflect a player using a Road Building Dev Card
      *
+
      * @return CatanModel, return the updated Catan model
      */
     @Override
