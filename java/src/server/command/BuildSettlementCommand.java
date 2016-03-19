8fd87d6f8c796824f68a78225e5fcb834a43fbce
Clayton Condie <clayton.condie@gmail.com>
2016-03-19 12:54:17 -0600

    Changes to buildSettlementCommand

--- java/src/server/command/BuildSettlementCommand.java
+++ java/src/server/command/BuildSettlementCommand.java
@@ -12,48 +12,47 @@ import shared.locations.VertexLocation;
 
 public class BuildSettlementCommand implements ICommand {
 
-	private AuthToken authToken = null;
-	private BuildSettlementJSON body = null;
-	private final IServerFacade facade;
-	
-	public BuildSettlementCommand(AuthToken authToken, IJavaJSON jsonBody, IServerFacade facade)
-	{
-		this.authToken = authToken;
-		this.body = (BuildSettlementJSON)jsonBody;
-		this.facade = facade;
-	}
+    private AuthToken authToken = null;
+    private BuildSettlementJSON body = null;
+    private final IServerFacade facade;
 
-	/**
-	 * 
-	 * Updates the CatanModel to reflect the building of a settlement
-	 * 
+    public BuildSettlementCommand(AuthToken authToken, IJavaJSON jsonBody, IServerFacade facade) {
+        this.authToken = authToken;
+        this.body = (BuildSettlementJSON) jsonBody;
+        this.facade = facade;
+    }
+
+    /**
+     * Updates the CatanModel to reflect the building of a settlement
+     *
      * @return CatanModel, return the updated Catan model
      */
-	@Override
-	public Object execute() {
-		
-		CatanModel cm = null; 
-		try
-		{
-			cm = facade.getGameModel(authToken);
+    @Override
+    public Object execute() {
 
-			int settleLocX = body.getVertexLocation().getX();
-			int settleLocY = body.getVertexLocation(). getY();
-			VertexDirection settleLocDir = VertexDirection.valueOf(body.getVertexLocation().getDirection());
-			VertexLocation settleLoc = new VertexLocation(new HexLocation(settleLocX, settleLocY), settleLocDir);
+        CatanModel cm = null;
+        try {
+            //Retrieve the game model
+            cm = facade.getGameModel(authToken);
 
-			cm.getMapManager().placeSettlement(settleLoc, body.getPlayerIndex());
-			facade.updateGame(authToken, cm);
-			
-		} catch (ServerException e)
-		{
-			e.printStackTrace();
-		}
-    	
-		return cm;
-		
-		
-		
-	}
+            //Translate from JSONbody into a Java settlement location
+            int settleLocX = body.getVertexLocation().getX();
+            int settleLocY = body.getVertexLocation().getY();
+            VertexDirection settleLocDir = VertexDirection.valueOf(body.getVertexLocation().getDirection());
+            VertexLocation settleLoc = new VertexLocation(new HexLocation(settleLocX, settleLocY), settleLocDir);
+
+            //Make change to the model
+            cm.getMapManager().placeSettlement(settleLoc, body.getPlayerIndex());
+
+            //Update the model with the facade
+            facade.updateGame(authToken, cm);
+
+        } catch (ServerException e) {
+            e.printStackTrace();
+        }
+
+        //Return the new model to the HTTP Handlers
+        return cm;
+    }
 
 }
