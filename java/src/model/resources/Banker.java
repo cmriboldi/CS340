7baf884507d663dac6d17ec896e06a0e7ac4afe6
Christian Riboldi <criboldi@familysearch.com>
2016-03-21 14:46:04 -0600

    Adds back some lost files.

---
+++ java/src/model/resources/Banker.java
@@ -0,0 +1,197 @@
+package model.resources;
+
+import java.util.ArrayList;
+
+import shared.definitions.Cost;
+import shared.definitions.PieceType;
+import shared.definitions.ResourceType;
+import shared.exceptions.resources.*;
+
+/**
+ * The Banker handles all transactions in or out of the Bank
+ * 
+ * @author Christian Riboldi
+ * @author Clayton Condie
+ * @author Jacob Brewer
+ * @author Joshua Powers
+ * @author Joshua Van Steeter
+ * @version 1.0 Build Jan, 2016.
+ */
+public class Banker
+{
+
+	private ResourceList bank;
+	private PlayerResources playerResources;
+	
+	public Banker(PlayerResources playerResources)
+	{
+		this.bank = new ResourceList(19,19,19,19,19);
+		this.playerResources = playerResources;
+	}
+
+	public Banker(PlayerResources playerResources, ResourceList bankResources)
+	{
+		this.bank = bankResources;
+		this.playerResources = playerResources;
+	}
+
+	/**
+	 * The banker pays out the desired resources to each of the players.
+	 * 
+	 * @param resLists An array of ResourceLists where the index of the ResourceList is the same as
+	 *            the index of the Player being paid.
+	 */
+	public void payPlayers(ArrayList<ResourceList> resLists) throws NotEnoughBankResourcesException
+	{
+		int index = 0;
+		for(ResourceList resList : resLists) 
+		{
+			if(!bank.greaterThan(resList))
+			{
+				throw new NotEnoughBankResourcesException("There are not enough resources in the bank to give to player " + index + ".");
+			}
+			
+			playerResources.addResourcesToPlayer(resList, index);
+			bank.minus(resList);
+			index++;
+		}
+	}
+
+	/**
+	 * This function is the implementation for buying pieces from the bank.
+	 * 
+	 * @param playerIndex The index of the player who is purchasing the piece.
+	 * @param piece The PieceType which will be bought.
+	 * @throws InvalidPieceTypeException 
+	 * @throws NotEnoughResourcesException 
+	 */
+	public void buyPiece(int playerIndex, PieceType piece) throws NotEnoughPlayerResourcesException, InvalidPieceTypeException, NotEnoughResourcesException
+	{
+		Cost product = null;
+		switch (piece)
+		{
+		case CITY:
+			product = Cost.CITY;
+			break;
+		case ROAD:
+			product = Cost.ROAD;
+			break;
+		case SETTLEMENT:
+			product = Cost.SETTLEMENT;
+			break;
+		default:
+			throw new InvalidPieceTypeException("Invalid piece trying to be purchased in the buyPiece function.");
+		}
+		
+		if(product == null || !canPlayerAfford(playerIndex, product))
+		{
+			throw new NotEnoughPlayerResourcesException("Not enough player resources to purchase this piece.");
+		}
+		playerResources.takeResourcesFromPlayer(product.getCost(), playerIndex);
+		bank.plus(product.getCost());
+	}
+
+	/**
+	 * Purchases a development card by paying the bank.
+	 * 
+	 * @param playerIndex Index of the player who is buying.
+	 * @throws NotEnoughResourcesException 
+	 */
+	public void buyDevCard(int playerIndex) throws NotEnoughPlayerResourcesException, NotEnoughResourcesException
+	{
+		if(!canPlayerAfford(playerIndex, Cost.DEVCARD))
+		{
+			throw new NotEnoughPlayerResourcesException("Not enough player resources to purchase a development card.");
+		}
+		playerResources.takeResourcesFromPlayer(Cost.DEVCARD.getCost(), playerIndex);
+		bank.plus(Cost.DEVCARD.getCost());
+	}
+
+	/**
+	 * This moves the requested resources to the player who is trading with the bank.
+	 * 
+	 * @param resList ResourceList with the positive numbers going to the player and the negative
+	 *            numbers coming from the Bank.
+	 * @param toPlayerIndex The index of the player who is trading with the bank.
+	 * @throws NotEnoughPlayerResourcesException 
+	 */
+	public void tradeWithBank(ResourceList resList, int toPlayerIndex) throws NotEnoughBankResourcesException, NotEnoughPlayerResourcesException
+	{
+		if(!bank.greaterThan(resList))
+		{
+			throw new NotEnoughBankResourcesException("There are not enough resources in the bank make that trade");
+		} else if(!playerResources.getResourcesForPlayer(toPlayerIndex).greaterThan(resList.invert()))
+		{
+			throw new NotEnoughPlayerResourcesException("The player doesn't have enough resources to offer that trade.");
+		}
+		playerResources.addResourcesToPlayer(resList, toPlayerIndex);
+		bank.minus(resList);
+	}
+
+	/**
+	 * @param playerIndex The index of the player who is using the card.
+	 * @param resourcesAskedFor The ResourceList with the two resources requested.
+	 * @throws InvalidNumberOfResourcesRequested 
+	 */
+	public void useYearOfPlentyCard(int playerIndex, ResourceList resourcesAskedFor)
+			throws NotEnoughBankResourcesException, InvalidNumberOfResourcesRequested
+	{
+		if (!bank.greaterThan(resourcesAskedFor))
+		{
+			throw new NotEnoughBankResourcesException("There are not enough resources in the bank for to use this monopoly card.");
+		} else if (resourcesAskedFor.getResourceCount() != 2)
+		{
+			throw new InvalidNumberOfResourcesRequested("You must request two resources when you use the year of plenty card.");
+		}
+		
+		playerResources.addResourcesToPlayer(resourcesAskedFor, playerIndex);
+		bank.minus(resourcesAskedFor);
+	}
+
+	public boolean canPlayerAfford(int playerIndex, Cost product)
+	{
+		return playerResources.canPlayerAfford(playerIndex, product.getCost());
+	}
+
+	public boolean canPlayerAfford(int playerIndex, ResourceList resourceList)
+	{
+		return playerResources.canPlayerAfford(playerIndex, resourceList);
+	}
+
+	public ResourceList getResourcesForPlayer(int playerIndex)
+	{
+		return playerResources.getResourcesForPlayer(playerIndex);
+	}
+
+	public int getResourceCount(int playerIndex, ResourceType resource)
+	{
+		return playerResources.getResourceCount(playerIndex, resource);
+	}
+
+	public int getTotalResourceCount(int playerIndex)
+	{
+		return playerResources.getTotalResourceCount(playerIndex);
+	}
+
+	public boolean bankHasResource(ResourceType resource)
+	{
+		return this.bank.getResourceTypeCount(resource) > 0;
+	}
+
+	public int getBankResourceCount(ResourceType resource)
+	{
+		return bank.getResourceTypeCount(resource);
+	}
+
+	public void discardCards(ResourceList discardedCards, int playerIndex) throws NotEnoughResourcesException
+	{
+		playerResources.takeResourcesFromPlayer(discardedCards, playerIndex);
+		bank.plus(discardedCards);
+	}
+
+	public int getGreatestCardCount()
+	{
+		return playerResources.getGreatestCardCount();
+	}
+
+}
---
+++ java/src/model/resources/ResourceManager.java
@@ -0,0 +1,249 @@
+package model.resources;
+
+import java.util.ArrayList;
+
+/** The ResourceManger class manages all transactions other packages need to make concerning resources.
+* @author Christian Riboldi
+* @author Clayton Condie
+* @author Jacob Brewer
+* @author Joshua Powers
+* @author Joshua Van Steeter
+* @version 1.0 Build Jan, 2016.
+*/
+
+import shared.definitions.*;
+import shared.exceptions.resources.*;
+import shared.exceptions.player.InvalidPlayerIndexException;
+
+public class ResourceManager
+{
+
+	private Banker banker = null;
+	private Trader trader = null;
+	private boolean[] hasPlayerDiscarded = null;
+
+	public ResourceManager()
+	{
+		PlayerResources newPlayerResources = new PlayerResources();
+		this.banker = new Banker(newPlayerResources);
+		this.trader = new Trader(newPlayerResources);
+		hasPlayerDiscarded = new boolean[] {false, false, false, false};
+	}
+	
+	public ResourceManager(ResourceList[] playerResources, ResourceList bankResources, TradeOffer tradeOffer, boolean[] hasPlayerDiscarded)
+	{
+		PlayerResources newPlayerResources = new PlayerResources(playerResources);
+		banker = new Banker(newPlayerResources, bankResources);
+		trader = new Trader(newPlayerResources, tradeOffer);
+		this.hasPlayerDiscarded = hasPlayerDiscarded;
+	}
+
+	/**
+	 * This function is the external access to purchasing a piece.
+	 * 
+	 * @param playerIndex The index of the player who is purchasing the piece.
+	 * @param piece The PieceType which will be bought.
+	 * @throws NotEnoughResourcesException 
+	 * @throws InvalidPieceTypeException 
+	 */
+	public void buyPiece(int playerIndex, PieceType piece) throws NotEnoughPlayerResourcesException, InvalidPieceTypeException, NotEnoughResourcesException
+	{
+		banker.buyPiece(playerIndex, piece);
+	}
+
+	/**
+	 * @param playerIndex The index of the player who is buying a development card.
+	 * @throws NotEnoughResourcesException 
+	 */
+	public void buyDevCard(int playerIndex) throws NotEnoughPlayerResourcesException, NotEnoughResourcesException
+	{
+		banker.buyDevCard(playerIndex);
+	}
+
+	/**
+	 * This is the external call for generating resources for the CatanModel.
+	 */
+	public void payOutResources(ArrayList<ResourceList> resLists) throws NotEnoughBankResourcesException
+	{
+		this.banker.payPlayers(resLists);
+	}
+
+	/**
+	 * Called when accepting the current Trade Offer.
+	 * 
+	 * @param int player index of the player accepting the trade offer. This index must be match the
+	 *            index of the player receiving the offer.
+	 * @throws NotEnoughPlayerResourcesException
+	 * @throws InvalidPlayerIndex
+	 */
+	public void acceptPlayerTrade(int playerIndex) throws NotEnoughPlayerResourcesException, InvalidPlayerIndexException, TradeOfferNullException
+	{
+		try
+		{
+			trader.acceptPlayerTrade(playerIndex);
+		} catch (Exception e)
+		{
+			throw e;
+		}
+		
+	}
+
+	/**
+	 * @param resList ResourceList with the negative numbers coming from the player and the positive
+	 *            numbers coming from the Bank.
+	 * @param toPlayerIndex The index of the player who is trading with the bank.
+	 * @throws NotEnoughPlayerResourcesException 
+	 */
+	public void tradeWithBank(ResourceList resList, int toPlayerIndex) throws NotEnoughBankResourcesException, NotEnoughPlayerResourcesException
+	{
+		banker.tradeWithBank(resList, toPlayerIndex);
+	}
+
+	/**
+	 * @param playerIndex The index of the player who is using the card.
+	 * @param resource The resource that the player is asking for.
+	 */
+	public void useMonopolyCard(int playerIndex, ResourceType resource)
+	{
+		trader.useMonopolyCard(playerIndex, resource);
+	}
+
+	/**
+	 * @param playerIndex The index of the player who is using the card.
+	 * @param resourcesAskedFor The ResourceList with the two resources requested.
+	 * @throws InvalidNumberOfResourcesRequested 
+	 */
+	public void useYearOfPlentyCard(int playerIndex, ResourceList resourcesAskedFor) throws NotEnoughBankResourcesException, InvalidNumberOfResourcesRequested
+	{
+		banker.useYearOfPlentyCard(playerIndex, resourcesAskedFor);
+	}
+
+	/**
+	 * Check if a player can afford a road
+	 * 
+	 * @param player index
+	 * @return True if allowed False otherwise
+	 */
+	public boolean canAffordRoad(int playerIndex)
+	{
+		return banker.canPlayerAfford(playerIndex, Cost.ROAD);
+	}
+
+	/**
+	 * Check if a player can afford a town
+	 * 
+	 * @param player index
+	 * @return True if allowed False otherwise
+	 */
+	public boolean canAffordTown(int playerIndex)
+	{
+		return banker.canPlayerAfford(playerIndex, Cost.SETTLEMENT);
+	}
+
+	/**
+	 * Check if a player can afford a city
+	 * 
+	 * @param player index
+	 * @return True if allowed False otherwise
+	 */
+	public boolean canAffordCity(int playerIndex)
+	{
+		return banker.canPlayerAfford(playerIndex, Cost.CITY);
+	}
+
+	/**
+	 * Check if a player can afford a development card
+	 * 
+	 * @param player index
+	 * @return True if allowed False otherwise
+	 */
+	public boolean canAffordDevCard(int playerIndex)
+	{
+		return banker.canPlayerAfford(playerIndex, Cost.DEVCARD);
+	}
+
+	public boolean canTrade(int playerIndex) throws TradeOfferNullException
+	{
+		return trader.canTrade(playerIndex);
+	}
+	
+	public boolean hasPlayerDiscarded(int playerIndex)
+	{
+		return hasPlayerDiscarded[playerIndex];
+	}
+
+	public void setHasPlayerDiscarded(boolean[] hasPlayerDiscarded)
+	{
+		this.hasPlayerDiscarded = hasPlayerDiscarded;
+	}
+
+	public boolean canAfford(int playerIndex, ResourceList resourceList)
+	{
+		return banker.canPlayerAfford(playerIndex, resourceList);
+	}
+	
+	public ResourceList getResourcesForPlayer(int playerIndex)
+	{
+		return banker.getResourcesForPlayer(playerIndex);
+	}
+	
+	public int getResourceCount(int playerIndex, ResourceType resource) {
+		return banker.getResourceCount(playerIndex, resource);
+	}
+	
+	public int getTotalResourceCount(int playerIndex) {
+		return banker.getTotalResourceCount(playerIndex);
+	}
+
+	public TradeOffer getTradeOffer()
+	{
+		return trader.getTradeOffer();
+	}
+
+	public boolean canAcceptTrade(int playerIndex)
+	{
+		return trader.canAcceptTrade(playerIndex);
+	}
+
+	public boolean bankHasResource(ResourceType resource)
+	{
+		return banker.bankHasResource(resource);
+	}
+	
+	public int getBankResourceCount(ResourceType resource)
+	{
+		return banker.getBankResourceCount(resource);
+	}
+
+	public void discardCards(ResourceList discardedCards, int playerIndex) throws NotEnoughResourcesException
+	{
+		banker.discardCards(discardedCards, playerIndex);
+	}
+
+	public void setTradeOffer(TradeOffer tradeOffer)
+	{
+		trader.setTradeOffer(tradeOffer);
+	}
+
+	public void declineTrade(int playerIndex)
+	{
+		trader.setTradeOffer(null);
+	}
+
+	public int getGreatestCardCount()
+	{
+		return banker.getGreatestCardCount();
+	}
+
+	public void setPlayerDiscard()
+	{
+		for(int i = 0; i < 4; i++) {
+			if(banker.getTotalResourceCount(i) > 7) {
+				hasPlayerDiscarded[i] = false;
+			} else {
+				hasPlayerDiscarded[i] = true;
+			}
+		}
+	}
+
+};
